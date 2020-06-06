9182c4867ea6cc957edb4fac30de004aad1afb74
Chul Chong <chulchong1030@gmail.com>
2020-06-05 23:06:51 -0400

    problem1315

--- problem1315.java
+++ problem1315.java
@@ -1,3 +1,19 @@
 class problem1315 {
+
   public static void main(String[] args) {}
+
+  int sum = 0;
+
+  public int sumEvenGrandparent(TreeNode root) {
+   
+  }
+
+  public void isGrandParentEven(TreeNode GrandParent,TreeNode Parent, TreeNode current) {
+    if(current==null){
+      return;
+    }
+    
+  }
+  
+  }
 }
