859f8366809a5e17fec4efb7d9278e4bfa2c1084
Chul Chong <chulchong1030@gmail.com>
2020-12-18 13:16:56 -0500

    problem1302

---
+++ problem1302.java
@@ -0,0 +1,7 @@
+public class problem1302 {
+  public static void main(String[] args) {}
+
+  public int deepestLeavesSum(TreeNode root) {}
+
+  public static int findLeaves(TreeNode current, TreeNode parent) {}
+}
