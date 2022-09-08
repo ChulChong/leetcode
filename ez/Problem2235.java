23be4394cf15c58304492e2ff414dd51b0b69bd0
Chul Chong <chulchong1030@gmail.com>
2022-09-08 09:27:31 -0400

    Problem2235

---
+++ ez/Problem2235.java
@@ -0,0 +1,11 @@
+public class Problem2235 {
+    public static void main(String[] args) {
+        int num1 = 12, num2 = 5;
+        System.out.println(sum(num1, num2));
+    }
+
+    public static int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+}
+
