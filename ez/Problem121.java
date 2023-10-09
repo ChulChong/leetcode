ed4cf87a3bfe2423d66c3154f74128922fd083ad
Chul Chong <62628427+ChulChong@users.noreply.github.com>
2023-10-09 16:31:10 -0400

    Problem 121 solved

--- ez/Problem121.java
+++ ez/Problem121.java
@@ -1,6 +1,6 @@
 public class Problem121 {
     public static void main(String[] args) {
-        int[] prices = {1,2};
+        int[] prices = {1, 2};
         System.out.println(maxProfit(prices));
 
         //https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
@@ -9,15 +9,15 @@ public class Problem121 {
     public static int maxProfit(int[] prices) {
         int min = Integer.MAX_VALUE;
         int profit = 0;
-        int curr = 0;
+        int index = 0;
 
-        for (int price : prices) {
-            if (price < min) {
-                min = price;
+        for (int i : prices) {
+            if (i < min) {
+                min = i;
             }
-            curr = price - min;
-            if (profit < curr) {
-                profit = curr;
+            index = i - min;
+            if (profit < index) {
+                profit = index;
             }
         }
         return profit;
