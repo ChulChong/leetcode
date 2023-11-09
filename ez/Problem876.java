dc3318340094ee1090fc58d8777eb07ec3dc7d8c
Chul Chong <62628427+ChulChong@users.noreply.github.com>
2023-11-09 17:18:59 -0500

    problem 876 solved

---
+++ ez/Problem876.java
@@ -0,0 +1,21 @@
+public class Problem876 {
+    public static void main(String[] args) {
+        ListNode n5 = new ListNode(5);
+        ListNode n4 = new ListNode(4, n5);
+        ListNode n3 = new ListNode(3, n4);
+        ListNode n2 = new ListNode(2, n3);
+        ListNode head = new ListNode(1, n2);
+
+        ListNode.printNode(middleNode(head));
+    }
+
+    public static ListNode middleNode(ListNode head) {
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+}
