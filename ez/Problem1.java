d93d813d0f8958a6bde17eb2d4f94e05a9c09a39
Chul Chong <62628427+ChulChong@users.noreply.github.com>
2023-11-09 19:07:07 -0500

    review problem1

--- ez/Problem1.java
+++ ez/Problem1.java
@@ -11,16 +11,16 @@ public class Problem1 {
 
     public static int[] twoSum(int[] nums, int target) {
         HashMap<Integer, Integer> hm = new HashMap<>();
-
         for (int i = 0; i < nums.length; i++) {
-            hm.put(nums[i], i);
-        }
-
-        for (int i = 0; i < nums.length; i++) {
-            if (hm.containsKey(target - nums[i]) && hm.get(target - nums[i]) != i) {
-                return new int[]{i, hm.get(target - nums[i])};
+            int left = target - nums[i];
+            if (hm.containsKey(left)) {
+                return new int[]{
+                        hm.get(left), i
+                };
+            } else {
+                hm.put(nums[i], i);
             }
         }
-        return new int[]{};
+        return new int[0];
     }
 }
