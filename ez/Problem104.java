3a2f13b490e3718646d3e413ce26cb72c3d79987
Chul Chong <62628427+ChulChong@users.noreply.github.com>
2025-08-04 19:10:47 -0400

    problem 104 solved

---
+++ ez/Problem104.java
@@ -0,0 +1,11 @@
+public class Problem104 {
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(3, new TreeNode(9), new TreeNode(20, new TreeNode(15), new TreeNode(7)));
+        System.out.println(maxDepth(root));
+    }
+
+    public static int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        return Math.max(maxDepth(root.left) + 1, maxDepth(root.right) + 1);
+    }
+}
